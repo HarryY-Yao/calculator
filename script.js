9d296469d8d8953b8cda3151841b953c00792d04
HarryY-Yao <[200~184984708+HarryY-Yao@users.noreply.github.com>
2025-01-24 10:51:17 -0500

    Add functionality to operation buttons

--- script.js
+++ script.js
@@ -26,9 +26,99 @@ const buttons = document.querySelectorAll(".btn");
 const numButtons = document.querySelectorAll(".num-btn");
 const onScreen = document.querySelector(".on-screen");
 const clear = document.querySelector("#AC");
+const operationBtns = document.querySelectorAll(".operator");
+const addBtn = document.querySelector("#add");
+const subtractBtn = document.querySelector("#subtract");
+const multiplyBtn = document.querySelector("#multiply");
+const divideBtn = document.querySelector("#divide");
+const equalsBtn = document.querySelector("#equals")
+
+
+addBtn.addEventListener("click", () => {
+    if (operation) {
+        number2 = Number(onScreen.textContent);
+
+        onScreen.textContent = operate(number1, number2, operation);
+        number1 = undefined;
+        number2 = undefined;
+    }
+    
+    operation = add;
+    
+    if (onScreen.textContent.length > 0) {
+        number1 = Number(onScreen.textContent);
+    }
+});
+
+subtractBtn.addEventListener("click", () => {
+    if (operation) {
+        number2 = Number(onScreen.textContent);
+        onScreen.textContent = operate(number1, number2, operation);
+        number1 = undefined;
+        number2 = undefined;
+    }
+    
+    operation = subtract;
+    
+    if (onScreen.textContent.length > 0) {
+        number1 = Number(onScreen.textContent);
+    }
+});
+
+divideBtn.addEventListener("click", () => {
+    if (operation) {
+        number2 = Number(onScreen.textContent);
+        onScreen.textContent = operate(number1, number2, operation);
+        number1 = undefined;
+        number2 = undefined;
+    }
+
+    operation = divide;
+    
+    if (onScreen.textContent.length > 0) {
+        number1 = Number(onScreen.textContent);
+    }
+});
+
+multiplyBtn.addEventListener("click", () => {
+    if (operation) {
+        number2 = Number(onScreen.textContent);
+        onScreen.textContent = operate(number1, number2, operation);
+        number1 = undefined;
+        number2 = undefined;
+    }
+    
+    operation = multiply;
+    
+    if (onScreen.textContent.length > 0) {
+        number1 = Number(onScreen.textContent);
+    }
+});
+
+equalsBtn.addEventListener("click", () => {
+    number2 = Number(onScreen.textContent);
+
+    if (number2 == 0 && operation == divide) {
+        onScreen.textContent = "ERROR"
+        
+    } else {
+    let result = operate(number1, number2, operation) + ""
+    if (result.length > 9) {
+        result = Math.round(Number(result) * 10 ** Math.abs(10 - result.length)) / 10 ** Math.abs(10 - result.length);
+    }
+    onScreen.textContent = result;
+    }
+    number1 = 0;
+    number2 = 0;
+    operation = null;
+});
 
 clear.addEventListener("click", () => {
-    onScreen.textContent = ""
+    onScreen.textContent = "";
+    number1 = 0;
+    number2 = 0;
+    operation = null;
+    storedValue = 0;
 });
 
 let display = function(button) {
@@ -39,8 +129,12 @@ let display = function(button) {
 numButtons.forEach((button) => {
 
     button.addEventListener("click", () =>{
-        if (onScreen.textContent.length < 10)
-        display(button);
+        if (onScreen.textContent.length < 10) {
+            if (operation) {
+                onScreen.textContent = "";
+            }
+        display(button)
+    }
     });
 
 
@@ -49,7 +143,7 @@ numButtons.forEach((button) => {
 buttons.forEach((button) => {
     button.addEventListener("mouseenter", () => {
         button.style.backgroundColor = "lightgray";
-
+    
     button.addEventListener("mouseleave", () => {
         button.style.backgroundColor = "";
     });
@@ -66,3 +160,4 @@ buttons.forEach((button) => {
       
     });
 });
+
